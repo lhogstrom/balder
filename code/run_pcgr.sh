404c295d04dc0d6608f2aaea50cc33a5fd9f192a
Larson Hogstrom <larsonhogstrom@Larsons-MacBook-Pro.local>
2022-11-15 15:24:28 -0500

    dir update

--- code/run_pcgr.sh
+++ code/run_pcgr.sh
@@ -5,11 +5,13 @@ inFile=$baseDir/samples2.txt
 
 while read p; do
 	echo $p
+	pcgrDir=$baseDir/${p}/purple/pcgr_out
+	mkdir $pcgrDir
 	inVCFgz=$baseDir/${p}/purple/${p}.pcgr_format.vcf.gz
 	inVCF=$baseDir/${p}/purple/${p}.pcgr_format.vcf
 	gunzip $inVCFgz
 	bgzip $inVCF
 	tabix $inVCFgz
-	pcgr --input_vcf /data/larsonh/hartwig/${p}/purple/${p}.pcgr_format.vcf.gz --output_dir /data/larsonh/hartwig/${p}/purple/testpcgr --pcgr_dir /data/sigven/pcgr --genome_assembly grch37 --sample_id $p --tumor_dp_tag TDP --tumor_af_tag TVAF --control_dp_tag CDP --control_af_tag CVAF --basic
+	pcgr --input_vcf /data/larsonh/hartwig/${p}/purple/${p}.pcgr_format.vcf.gz --output_dir $pcgrDir --pcgr_dir /data/sigven/pcgr --genome_assembly grch37 --sample_id $p --tumor_dp_tag TDP --tumor_af_tag TVAF --control_dp_tag CDP --control_af_tag CVAF --basic
 done <$inFile
 
