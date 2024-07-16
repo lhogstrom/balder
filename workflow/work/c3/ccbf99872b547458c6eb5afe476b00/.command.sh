159a27767c8310bd4e743e8b7891891a41c55cae
Larson Hogstrom <larson.hogstrom@gmail.com>
2024-07-16 11:49:00 -0400

    removing nextflow work dir output

--- workflow/work/c3/ccbf99872b547458c6eb5afe476b00/.command.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash -ue
-Rscript ../code/clinical_annotation_db_curration.r CIViC-01-Dec-2021-ClinicalEvidenceSummaries.tsv ../../data/processed/balderResultsDb/balder-harmonized-biomarker-data-v20240712.sqlite
